daa2ec073daff6af142f613c181bb8547f4a17a6
backpaqer <backpaqer@users.noreply.github.com>
2023-01-07 12:20:52 +1100

    Update config.php

--- website/php/config.php
+++ website/php/config.php
@@ -1,12 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: ZEPHYR
- * Date: 31-Mar-19
- * Time: 11:29 AM
  */
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'poolboy');
-    define('DB_PASSWORD', 'poolpass');
+    define('DB_USERNAME', '{username}');
+    define('DB_PASSWORD', '{userpass}');
     define('DB_DATABASE', 'poolboy');
     $connect = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
